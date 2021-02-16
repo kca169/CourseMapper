fc16d78c9cedc797b3fbeeb3dc9d819ba2748458
Benjamin Hall <bvhall@sfu.ca>
2021-02-15 21:33:35 -0800

    login fix (hopefully)

--- coursemapper/settings.py
+++ coursemapper/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'coursemapper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR.joinpath('registration'))],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
