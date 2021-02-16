80a79f90509f8fa8590ba73115e455ccdca67af3
kca169 <kca169@sfu.ca>
2021-02-15 20:38:11 -0800

    login

--- coursemapper/settings.py
+++ coursemapper/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'coursemapper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('registration'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
