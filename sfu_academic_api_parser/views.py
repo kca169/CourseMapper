65f292a87f79a97e6841bf5dac841a15458462aa
kca169 <kca169@sfu.ca>
2021-02-23 22:07:54 -0800

    Added directions.html and hardcoded parsing abilities

--- sfu_academic_api_parser/views.py
+++ sfu_academic_api_parser/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from .models import Course
+from django.template import loader 
+import requests 
 
-def index(request):
-    return HttpResponse("Hello, world! You have made it to the sfu_academic_parser!")
+#This function GET's from the provided url, turns the resuld into JSON then loads and renders directions.html
+def prereqs(request):
+    response = requests.get('http://www.sfu.ca/bin/wcm/academic-calendar?2020/spring/courses/cmpt/307')
+    courses = response.json()
+    template = loader.get_template('sfu_academic_api_parser/directions.html')
+    context = {'courses':courses,}
+    return HttpResponse(template.render(context,request))
