16ed8c4f3596882aa3210917190a3a3d9bcc8e06
Benjamin Hall <benvahall@gmail.com>
2021-02-25 16:21:08 -0800

    initial model changes

--- sfu_academic_api_parser/models.py
+++ sfu_academic_api_parser/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Course(models.Model):
-    department = models.CharField(max_length=25)
-    number = models.CharField(max_length=10)  ##This is not a IntegerField because some courses contain letters (ex. CMPT 105W)
+    course_code = models.CharField(max_length=25)
+    number_raw = models.CharField(max_length=10)  ##This is not a IntegerField because some courses contain letters (ex. CMPT 105W)
+    number_extracted = models.IntegerField(max_length=10)
+    is_w_course = models.BooleanField()
+    units = models.IntegerField()
     description = models.CharField(max_length=1000)
 
     def __str__(self):
-        return self.department + self.number 
+        return to_upper(self.department) + self.number 
