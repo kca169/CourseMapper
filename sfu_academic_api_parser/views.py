ae5675e1cc4eaae0ca70564aad62468dc2b7cdfa
psmattu <psmattu@sfu.ca>
2021-02-25 16:03:26 -0800

    course search

--- sfu_academic_api_parser/views.py
+++ sfu_academic_api_parser/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Course
 from django.template import loader 
+from .forms import CourseForm
 import requests 
 
 #This function GET's from the provided url, turns the resuld into JSON then loads and renders directions.html
+
 def prereqs(request):
-    dep = 'cmpt'
-    num = '300'
+    dep = request.POST.get('department', '')
+    num = request.POST.get('course_number', '')
     url = 'http://www.sfu.ca/bin/wcm/academic-calendar?2020/spring/courses/' + dep + '/' + num
     url = requests.get(url)
     courses = url.json()
