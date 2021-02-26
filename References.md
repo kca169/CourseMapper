11c2fd05c3613d646f482f45a3356c1aa8c94435
Benjamin Hall <benvahall@gmail.com>
2021-02-25 16:51:35 -0800

    made minor tweaks to template and added block comment in models.py

--- References.md
+++ References.md
@@ -1,6 +1,8 @@
 # CourseMapper Project References
 
-## In code citations appear as a comment in the form `Ref: [#]`, where the reference number is inside the brackets, following the IEEE convention.
+## In code citations appear as a comment in the form `Refs: [#]`, where the reference number is inside the brackets, following the IEEE convention.
 
 [1] https://learndjango.com/tutorials/django-login-and-logout-tutorial
 [2] https://www.toptal.com/developers/gitignore/api/django,python
+[3] https://docs.djangoproject.com/en/3.1/intro/tutorial02/
+[4] https://docs.djangoproject.com/en/3.1/ref/models/fields/
--- sfu_academic_api_parser/models.py
+++ sfu_academic_api_parser/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
+# Refs : [3],[4], 
+
+
+'''
+THE BASIC COURSE MODEL:
+
+Things to add:
+    WQB status
+'''
+
 class Course(models.Model):
-    course_code = models.CharField(max_length=25)
-    number_raw = models.CharField(max_length=10)  ##This is not a IntegerField because some courses contain letters (ex. CMPT 105W)
-    number_extracted = models.IntegerField(max_length=10)
+    
+    dept_code = models.CharField(max_length=25)
+    number_raw = models.CharField(max_length=10)  ##This is not initally IntegerField because some courses contain letters (ex. CMPT 105W)
+    number_extracted = models.IntegerField() # This is the course number
     is_w_course = models.BooleanField()
     units = models.IntegerField()
     description = models.CharField(max_length=1000)
 
+
     def __str__(self):
-        return to_upper(self.department) + self.number 
+        return self.department + self.number 
