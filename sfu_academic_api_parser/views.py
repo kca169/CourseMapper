68e11b169838d8e2bbf9ba5c2d4696d4b1b0fad5
Benjamin Hall <bvhall@sfu.ca>
2021-02-26 16:45:52 -0800

    got it! Now can search by year and semester, and outputs the title, description and pre-reqs

--- sfu_academic_api_parser/views.py
+++ sfu_academic_api_parser/views.py
@@ -8,16 +8,25 @@ import requests
 #This function GET's from the provided url, turns the resuld into JSON then loads and renders directions.html
 
 def prereqs(request):
-    dep = request.POST.get('department', '')
-    num = request.POST.get('course_number', '')
+    
     year = request.POST.get('year', '')
     semester = request.POST.get('semester', '')
+    dep = request.POST.get('department', '')
+    num = request.POST.get('course_number', '')
+    
+    print(dep)
+    print(num)
+    print(year)
+    print(semester)
 
-    url = 'http://www.sfu.ca/bin/wcm/academic-calendar?{year}/{semester}/courses/{dep}/{num}' # changed to formatted string
+    url = f'http://www.sfu.ca/bin/wcm/academic-calendar?{year}/{semester}/courses/{dep}/{num}' # changed to formatted string
+    url_raw = url
     url = requests.get(url)
     courses = url.json()
     template = loader.get_template('sfu_academic_api_parser/directions.html')
     context = {'courses':courses,}
+    print(url.status_code) #debug
+    print(url_raw)
     return HttpResponse(template.render(context,request))
 
 '''def prereqs(request):
