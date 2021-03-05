47d2b91445dd7e9221b8b51eac77c437cef258c8
Benjamin Hall <bvhall@sfu.ca>
2021-03-05 13:55:48 -0800

    Tried a many-to-one implementation. Seems to work well for a course having a single pre-requisite, but otherwise seems limited. Will try a many-to-many relationship next

--- sfu_academic_api_parser/models.py
+++ sfu_academic_api_parser/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
+# Refs: [8]
+
 # Basic course model.
 class Course(models.Model):
 
     number_str = models.CharField(max_length=12, default="TEST 123")  ##This is not a IntegerField because some courses contain letters (ex. CMPT 105W). See course_number.
     description = models.CharField(max_length=1000, default="This is an example description")
 
-    code = models.CharField(max_length=5) ## The (usually) four letter string that preceds the course number
+    code = models.CharField(max_length=5, default="TEST") ## The (usually) four letter string that preceds the course number
     number = models.IntegerField()
 
-    # prerequisites = ??? # NEED TO FIND A datatype / relationship system for this.
+    prerequisites = models.ForeignKey('self', null=True, on_delete=models.CASCADE) # Find alterative to CASCADE
 
     def __str__(self):
-        return self.department + self.number 
+        return self.number_str + self.description
     
     pass
