a1ec688bd16c59ed33b27343d5784604c7d3c68c
Benjamin Hall <bvhall@sfu.ca>
2021-03-04 21:28:28 -0800

    Tested console fields. Most work, though I have yet to found a suitable representation for course pre-reqs. Will investigate.

--- sfu_academic_api_parser/models.py
+++ sfu_academic_api_parser/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Basic course model.
 class Course(models.Model):
-    department = models.CharField(max_length=25)
-    number = models.CharField(max_length=10)  ##This is not a IntegerField because some courses contain letters (ex. CMPT 105W)
-    description = models.CharField(max_length=1000)
+
+    number_str = models.CharField(max_length=12, default="TEST 123")  ##This is not a IntegerField because some courses contain letters (ex. CMPT 105W). See course_number.
+    description = models.CharField(max_length=1000, default="This is an example description")
+
+    code = models.CharField(max_length=5) ## The (usually) four letter string that preceds the course number
+    number = models.IntegerField()
+
+    # prerequisites = ??? # NEED TO FIND A datatype / relationship system for this.
 
     def __str__(self):
         return self.department + self.number 
+    
+    pass
