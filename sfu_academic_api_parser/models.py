2b7ab29628aeeceed9691115e7176de5ece7b01a
kca169 <kca169@sfu.ca>
2021-03-07 21:18:20 -0800

    added commented out code which will possibly be used to get api data into our database

--- sfu_academic_api_parser/models.py
+++ sfu_academic_api_parser/models.py
@@ -5,14 +5,13 @@ from django.db import models
 # Basic course model.
 class Course(models.Model):
 
+    title = models.CharField(max_length = 50, default='This is an example title')
     number_str = models.CharField(max_length=12, default="TEST 123")  ##This is not a IntegerField because some courses contain letters (ex. CMPT 105W). See course_number.
     description = models.CharField(max_length=1000, default="This is an example description")
-
     code = models.CharField(max_length=5, default="TEST") ## The (usually) four letter string that preceds the course number
-    number = models.IntegerField()
-
+    #number = models.IntegerField()
     prerequisites = models.ForeignKey('self', null=True, on_delete=models.CASCADE) # Find alterative to CASCADE
-
+    units = models.IntegerField()
     def __str__(self):
         return self.number_str + self.description
     
--- sfu_academic_api_parser/views.py
+++ sfu_academic_api_parser/views.py
@@ -5,7 +5,10 @@ from .models import Course
 from django.template import loader 
 
 #from .forms import CourseForm
-import requests 
+import requests
+
+#import json
+
 
 #This function GET's from the provided url, turns the resuld into JSON then loads and renders directions.html
 
@@ -13,28 +16,50 @@ import requests
 
 def prereqs(request):
     
-    # getting form data, convert all to lower-case as that is how the API serves the data.
+    # all_courses={}
+     
+    ## getting form data, convert all to lower-case as that is how the API serves the data.
     year = request.POST.get('year', '').lower()
     semester = request.POST.get('semester', '').lower()
     dep = request.POST.get('department', '').lower()
     num = request.POST.get('course_number', '').lower()
     
     # DEBUG
-    '''print(dep)
-    print(num)
-    print(year)
-    print(semester)'''
+    # '''print(dep)
+    # print(num)
+    # print(year)
+    # print(semester)'''
 
     url = f'http://www.sfu.ca/bin/wcm/academic-calendar?{year}/{semester}/courses/{dep}/{num}' # changed to formatted string
     url_raw = url
     url = requests.get(url)
     courses = url.json()
+    
+    ## Convert the response from above to a string then load the string and store in a variable called data
+    # courses_str = json.dumps(courses)
+    # data = json.loads(courses_str)
+
+    
+    ##loop through our list of courses obtained above and assign the parameters as fields from the Course model
+    # for i in data:
+    #         course_data = Course(
+    #         title=i['title'],
+    #         number_str=i['number'],
+    #         description=i['description'],
+    #         prerequisites=i['prerequisites'],
+    #         units=i['units']
+    #     )
+    #         course_data.save()
+    #         all_courses = Course.objects.all()
+    
+    
     template = loader.get_template('sfu_academic_api_parser/directions.html')
     context = {'courses':courses,}
 
-    if url.status_code != 200:
+    if url.status_code != 200: 
         raise Http404("Cannot find course")
     
-    # print(url.status_code) #debug
-    # print(url_raw)
+    ## print(url.status_code) #debug
+    ## print(url_raw)
     return render(request, 'sfu_academic_api_parser/directions.html', context)
+    # return render(request, 'sfu_academic_api_parser/directions.html', {"all_courses":all_courses})
