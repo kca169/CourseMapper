e320effec93bbcafc205587743c2324c93fada83
Benjamin Hall <bvhall@sfu.ca>
2021-03-12 13:33:48 -0800

    added stub functions

--- sfu_academic_api_parser/tests.py
+++ sfu_academic_api_parser/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from sfu_academic_api_parser.models import Course
+from sfu_academic_api_parser.views import manual_input
+from sfu_academic_api_parser.views import search
+# Refs: [9]
+
+# View tests
+class Create_Courses(TestCase):
+     print("Hello world!") # Placeholder
+
+
+# Model tests
+# Tests that duplicate courses cannot be added
+class Test_Uniqueness(TestCase):
+
+    def initialize_test(self):
+        # Create object
+        Course.objects.create()
