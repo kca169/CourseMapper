03c6cd8635636aa3f473c11eae13e23771a47510
bvhall <ben_hall_2@sfu.ca>
2021-03-12 20:59:30 -0800

    Revert "Merge branch 'revert-2719ec8b' into 'master'"
    This reverts merge request !27

--- sfu_academic_api_parser/urls.py
+++ sfu_academic_api_parser/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.prereqs, name='prereqs'),
+    path('manual_input/', views.manual_input, name='manual_input'),
+    path('directions/', views.directions, name='directions'),
+    path('database_search/', views.search, name='database_search'),
+    path('error/', views.search, name='error',),
 ]
